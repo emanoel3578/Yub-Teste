a3e21de80c361e2a3290dd9b521ab4e2cabf97b2
emanoel3578 <emanoel357@gmail.com>
2021-09-25 10:33:17 -0300

    fazendo commit antes de fazer a migração das tabelas de estoque

---
+++ app/Models/Estoque.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estoque extends Model
+{
+    use HasFactory;
+
+    /**
+     * Protected fillabes to avoid mass assigment
+     * 
+     * @return string[]
+     */
+    protected $fillable=[
+        ''
+    ];
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'd/m/Y';
+
+    const CREATED_AT = 'criado_em';
+    const UPDATED_AT = 'atualizado_em';
+}
---
+++ database/migrations/2021_09_25_132119_create_estoques_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstoquesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estoques', function (Blueprint $table) {
+            $table->id();
+            $table->string('cadastro_id');
+            $table->string('tipo');
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estoques');
+    }
+}
